2782c764760843dee6e6080677a42d8357d5a941
arpitagarwal <arpit.agarwal@cybernetyx.com>
2020-11-12 18:56:27 +0530

    Post Item UI implementation

--- app/src/main/java/com/arpit/twitterclone/ui/viewholder/PostViewHolder.kt
+++ app/src/main/java/com/arpit/twitterclone/ui/viewholder/PostViewHolder.kt
@@ -15,11 +15,16 @@ import com.bumptech.glide.Glide
 class PostViewHolder(private val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(post: Post, onItemClicked: (Post, ImageView) -> Unit) {
-        binding.postTitle.text = post.name
-        binding.postAuthor.text = post.id.toString()
+        binding.name.text = post.name
+        binding.handle.text = post.handle
+        binding.likeCount.text = post.favoriteCount.toString()
+        binding.retweetCount.text = post.retweetCount.toString()
+        binding.description.text = post.text
+
 
         Glide.with(binding.imageView.context)
             .load(post.profileImageUrl)
+            .circleCrop()
             .placeholder(R.drawable.ic_person_24dp)
             .error(R.drawable.ic_person_24dp)
             .into(binding.imageView)
